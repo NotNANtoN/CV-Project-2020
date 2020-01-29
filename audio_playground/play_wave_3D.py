fdf66d90c0dc52d7aeee118908816168665c9446
Marius Pierenkemper <5pierenk@informatik.uni-hamburg.de>
2020-01-29 22:22:04 +0100

    Audio playground: small update

--- audio_playground/play_wave_3D.py
+++ audio_playground/play_wave_3D.py
@@ -5,7 +5,7 @@ import time
 import pyautogui
 
 volume_factor = 0.02
-pitch_factor = 0.001
+pitch_factor = 0.01
 
 source = openal.oalOpen("sound.wav")
 source.play()
@@ -18,12 +18,17 @@ while True:
     
     relative_x, relative_y = curr_x - mouse_x, curr_y - mouse_y
     
-    source.set_position([relative_x * volume_factor, relative_y * volume_factor, 0])
-    source.set_pitch(1 + (abs(relative_x) + abs(relative_y)) * pitch_factor)
+    relative_x *= volume_factor
+    relative_y *= volume_factor
+    
+    pitch = 1 - (abs(relative_x) + abs(relative_y)) * pitch_factor
+    
+    source.set_position([relative_x, relative_y, 0])
+    source.set_pitch(pitch)
     
     time.sleep(0.01)
     
-    print("X: {}, Y: {}".format(relative_x, relative_y))
+    print("Pos(X: {:.2f}, Y: {:.2f}), Pitch({:.2f})".format(relative_x, relative_y, pitch))
 
     if source.get_state() != openal.AL_PLAYING:
         source.play()
