7be58b1d7429a9c367231099c83aa9861dfeb19b
Marius <marius.pierenkemper@gmail.com>
2020-05-14 14:38:27 +0200

    Audio playground 3D movement

---
+++ audio_playground/play_wave_2D.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import openal
+import time
+import pyautogui
+
+volume_factor = 0.02
+pitch_factor = 0.001
+
+source = openal.oalOpen("sound.wav")
+source.play()
+
+[mouse_x, mouse_y] = pyautogui.position()
+
+while True:
+    
+    [curr_x, curr_y] = pyautogui.position()
+    
+    relative_x, relative_y = curr_x - mouse_x, curr_y - mouse_y
+    
+    source.set_position([relative_x * volume_factor, relative_y * volume_factor, 0])
+    source.set_pitch(1 + (abs(relative_x) + abs(relative_y)) * pitch_factor)
+    
+    time.sleep(0.01)
+    
+    print("X: {}, Y: {}".format(relative_x, relative_y))
+
+    if source.get_state() != openal.AL_PLAYING:
+        source.play()
+
+openal.oalQuit()
--- audio_playground/play_wave_3D.py
+++ audio_playground/play_wave_3D.py
@@ -3,9 +3,13 @@
 import openal
 import time
 import pyautogui
+from msvcrt import getch, kbhit
 
-volume_factor = 0.02
+mouse_scale = 0.02
 pitch_factor = 0.001
+
+z_pos = 0.0
+z_speed = 0.5
 
 source = openal.oalOpen("sound.wav")
 source.play()
@@ -13,17 +17,26 @@ source.play()
 [mouse_x, mouse_y] = pyautogui.position()
 
 while True:
+    if kbhit():
+        key = ord(getch())
+
+        if key == 80: #down-arrow
+                z_pos += z_speed
+        elif key == 72: #up-arrow
+                z_pos -= z_speed
     
     [curr_x, curr_y] = pyautogui.position()
     
     relative_x, relative_y = curr_x - mouse_x, curr_y - mouse_y
+
+    x_pos, y_pos = relative_x * mouse_scale, relative_y * mouse_scale
     
-    source.set_position([relative_x * volume_factor, relative_y * volume_factor, 0])
+    source.set_position([x_pos, y_pos, z_pos])
     source.set_pitch(1 + (abs(relative_x) + abs(relative_y)) * pitch_factor)
     
     time.sleep(0.01)
     
-    print("X: {}, Y: {}".format(relative_x, relative_y))
+    print("X: {}, Y: {}, Z: {}".format(x_pos, y_pos, z_pos))
 
     if source.get_state() != openal.AL_PLAYING:
         source.play()
