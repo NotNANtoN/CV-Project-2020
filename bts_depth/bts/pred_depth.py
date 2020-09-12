ebb0e0a41a4dcdde32ba12a8e9cb800aa1da5a7d
NotAnton <antonwiehe@gmail.com>
2020-09-12 12:25:24 +0200

    added rmse calculation

--- bts_depth/bts/pred_depth.py
+++ bts_depth/bts/pred_depth.py
@@ -5,7 +5,27 @@ ds = PoseDataset(mode="test") # mode = train / test
 
 img, depth, label, calibrate_params = ds[0]
 
-print(img.shape, depth.shape, label, calibrate_params)
+print(len(ds), img.shape, depth.shape, label, calibrate_params)
 
 parser = None
 depth_model = BTS(parser)
+depth_model.cuda()
+
+
+targets = []
+preds = []
+
+for img, depth, label, calibrate_params in ds:
+    img = torch.tensor(img).cuda()
+    depth = torch.tensor(depth)
+    pred_depth = depth_model.forward(img)
+    
+    targets.append(depth.cpu())
+    preds.append(pred_depth.cpu())
+    
+targets = torch.stack(targets)
+preds = torch.stack(preds)
+
+rmse = (targets - preds).pow(2).sum().sqrt()
+print("RMSE: ", rmse)
+    
